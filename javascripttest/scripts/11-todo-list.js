91aa20ea1d54119b3b2649b5909d1f28a7dd81e5
Paul <pauljuliuswies@gmail.com>
2025-07-18 18:08:59 +0200

    kp
    update

--- javascripttest/scripts/11-todo-list.js
+++ javascripttest/scripts/11-todo-list.js
@@ -1,8 +1,6 @@
 let tasklist = []
-
 let taskinput = ""
 let dateinput = ""
-
 
 function ADDTODO () {
 taskinput = document.querySelector('.TASKINPUT').value
@@ -16,20 +14,15 @@ console.log(tasklist);
 DISPLAYARRAY()}
 }
 
-
 function DISPLAYARRAY () {
-
 document.querySelector('.JSTODO').innerHTML =""
-for (i = 0 ; i<tasklist.length ; i++)
-document.querySelector('.JSTODO').innerHTML += `<p>${tasklist[i].task}</p> <p>${tasklist[i].date}</p> <button onclick="REMOVEDATA(${[i]})">delete</button><br>`
 
+for (i = 0 ; i<tasklist.length ; i++)
+document.querySelector('.JSTODO').innerHTML += `<p>${tasklist[i].task}</p> <p>${tasklist[i].date}</p> <button onclick="REMOVEDATA(${[i]})">delete</button><br>` 
 }
 
 function REMOVEDATA (position) {
-
 tasklist.splice(position,1)
 DISPLAYARRAY()
-
-
 }
 
