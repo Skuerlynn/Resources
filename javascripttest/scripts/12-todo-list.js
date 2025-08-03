adcde787108dc921e1c11abdfb75e59991b7c95c
Paul <pauljuliuswies@gmail.com>
2025-08-04 00:41:09 +0200

    verbesserungen
    onclick zu addEventHandler , neue loops (.map und .filter)

---
+++ javascripttest/scripts/12-todo-list.js
@@ -0,0 +1,28 @@
+let tasklist = []
+let taskinput = ""
+let dateinput = ""
+
+function ADDTODO () {
+taskinput = document.querySelector('.TASKINPUT').value
+dateinput = document.querySelector('.DATEINPUT').value
+
+if (taskinput && dateinput != "") {
+tasklist.push({ task: taskinput, date: dateinput });
+
+taskinput = ""
+console.log(tasklist);
+DISPLAYARRAY()}
+}
+
+function DISPLAYARRAY () {
+document.querySelector('.JSTODO').innerHTML =""
+
+for (i = 0 ; i<tasklist.length ; i++)
+document.querySelector('.JSTODO').innerHTML += `<p>${tasklist[i].task}</p> <p>${tasklist[i].date}</p> <button onclick="REMOVEDATA(${[i]})">delete</button><br>` 
+}
+
+function REMOVEDATA (position) {
+tasklist.splice(position,1)
+DISPLAYARRAY()
+}
+
